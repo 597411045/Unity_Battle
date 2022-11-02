ec0d4982d10a58bfdc16a0f3ddf071535543bc45
597411045 <60997269+597411045@users.noreply.github.com>
2022-11-02 23:41:14 +0800

    Add UAV

---
+++ Assets/UAV_BodyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UAV_BodyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.name.Contains("UAVHand")) return;
+        Transform UVAHand = other.transform.GetChild(0);
+
+        if (Vector3.Angle(UVAHand.forward, this.transform.forward) > 10) return;
+        if (Vector3.Angle(UVAHand.up, -this.transform.right) > 10) return;
+        if (Vector3.Angle(UVAHand.right, this.transform.up) > 10) return;
+
+        UVAHand.SetParent(this.transform.parent);
+        UVAHand.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/UAV_HandScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UAV_HandScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.gameObject.name.Contains("UAVFan")) return;
+        Transform UVAFan = other.transform.GetChild(0);
+
+        if (Vector3.Angle(UVAFan.forward, this.transform.GetChild(0).forward) > 10) return;
+        if (Vector3.Angle(UVAFan.up, this.transform.GetChild(0).up) > 10) return;
+        if (Vector3.Angle(UVAFan.right, this.transform.GetChild(0).right) > 10) return;
+
+        UVAFan.SetParent(this.transform.GetChild(0));
+        UVAFan.localPosition = Vector3.zero;
+    }
+}
